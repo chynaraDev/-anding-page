75fb4674092981a34d5dd7795e4777f5556954c9
chynaraDev <batyrovnach@gmail.com>
2022-12-30 19:50:49 +0600

    initial

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+    slidesPerView: 1,
     loop:true,
-    centeredSlides: true,
-    spaceBetween: 30,
-    grabCursor: true,
+    spaceBetween: 10,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 50,
+        },
     },
 });
